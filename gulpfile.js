37d1dccc61190a6d3f522e7bd20b0fd96b70a28f
davidgoli <davigoli@gmail.com>
2017-04-29 16:00:59 -0700

    deploy with rsync

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,11 @@
 'use strict';
 
-var gulp = require('gulp');
+var gulp   = require('gulp');
+var gutil  = require('gulp-util');
+var argv   = require('minimist')(process.argv);
+var gulpif = require('gulp-if');
+var prompt = require('gulp-prompt');
+var rsync  = require('gulp-rsync');
 
 gulp.paths = {
   src: 'src',
@@ -15,3 +20,32 @@ gulp.task('default', ['clean'], function () {
     gulp.start('build');
 });
 
+gulp.task('deploy', function () {
+  var rsyncPaths = ['dist', 'src', 'e2e', 'assets'];
+
+  // Default options for rsync
+  var rsyncConf = {
+    progress: true,
+    incremental: true,
+    relative: true,
+    emptyDirectories: true,
+    recursive: true,
+    clean: true,
+    exclude: [],
+  };
+
+  rsyncConf.hostname = 'wastebuddy.us'; // hostname
+  rsyncConf.username = 'wbdeploy'; // ssh username
+  rsyncConf.destination = '/home/david/webapps/wastebuddy'; // path where uploaded files go
+
+  // Use gulp-rsync to sync the files
+  return gulp.src(rsyncPaths)
+    .pipe(rsync(rsyncConf));
+});
+
+function throwError(taskName, msg) {
+  throw new gutil.PluginError({
+      plugin: taskName,
+      message: msg
+    });
+}
